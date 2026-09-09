2be13c42d6be669625520f4d863f1a7261cbe2f4
61ackPink <v61ackPink@163.com>
2026-09-09 15:52:23 +0800

    图片删除

--- apps/tools/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
--*- coding: utf-8 -*-
-@File  : __init__.py.py
-@Author: 61ackPink
-@Time : 2026/8/27 17:16
-@Desc : 
-"""
--- apps/tools/urls.py
+++
@@ -1,7 +0,0 @@
-"""
--*- coding: utf-8 -*-
-@File  : urls.py
-@Author: 61ackPink
-@Time : 2026/8/27 17:19
-@Desc : 
-"""
